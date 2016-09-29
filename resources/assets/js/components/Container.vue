325e3746ddbb92412c1292f7195af6ea1b991316
Gareth Nicholson <gareth.nic@gmail.com>
2016-09-29 11:41:15 +0200

    initial commit

---
+++ resources/assets/js/components/Container.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="app">
+        <income></income>
+        <expense></expense>
+        <div>
+            <table class="overview-table">
+                <tr>
+                    <th>Item</th>
+                    <th>Amount</th>
+                </tr>
+                <tr>
+                    <td>Income</td>
+                    <td>{{ income }}</td>
+                </tr>
+                <tr>
+                    <td>Expenses</td>
+                    <td>{{ expenses }}</td>
+                </tr>
+                <tr>
+                    <td>Net</td>
+                    <td>{{ leftOver }}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Income from './Income.vue';
+    import Expense from './Expenses.vue';
+
+    export default {
+        data() {
+            return {
+                income: 0,
+                expenses: 0
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+          leftOver: function () {
+              return this.income - this.expenses;
+          }
+        },
+        components: {
+            'income': Income,
+            'expense': Expense
+        },
+        events: {
+            'income-total': function (total) {
+                this.income = total;
+            },
+            'expenses-total': function (total) {
+                this.expenses = total;
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/Income.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="header-title header-positive">Income</div>
+
+        <div class="form-holder">
+            <input type="text" name="incomeName" v-model="name" id="incomeItem" placeholder="Add Item" />
+            <input type="text" name="incomeAmount" v-model="amount" id=incomeAmount" placeholder="Add Amount" />
+            <button v-on:click="add">Add</button>
+        </div>
+
+        <div v-if="items.length > 0">
+            <table class="items-table">
+                <tr>
+                    <th>Name</th>
+                    <th>Amount</th>
+                </tr>
+                <tr v-for="item in items">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.amount }}</td>
+                    <td><button v-on:click="removeItem($index)" class="remove-button">X</button></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Income',
+
+        data() {
+            return {
+                name: '',
+                amount: '',
+                items: []
+            }
+        },
+        methods: {
+            add: function (e) {
+                let holdItems = {};
+                holdItems.name = this.name;
+                holdItems.amount = this.amount;
+
+                this.items.push(holdItems);
+                this.name = '';
+                this.amount = '';
+                this.updateTotal();
+            },
+            removeItem: function (item) {
+                this.items.splice(item, 1);
+                this.updateTotal();
+            },
+            updateTotal: function () {
+                let total = 0;
+                this.items.forEach(item => {
+                   total += parseInt(item.amount);
+                });
+
+                this.$dispatch('income-total', total);
+            }
+        }
+    }
+</script>
