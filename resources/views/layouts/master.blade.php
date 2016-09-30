a8e4b671f0555bee2184bf400abed73e0502a05a
Gareth Nicholson <gareth.nic@gmail.com>
2016-09-30 14:01:26 +0200

    update meta tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="A simple way to keep track of your monthly expenses">
+        <meta name="keywords" content="income,expenses,money,overview,amount,net,moneymath">
 
         <title>MoneyMath</title>
 
