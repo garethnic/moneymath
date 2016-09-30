f8e7acc850f73846682ebc7a801a0dbe5ec1a3f2
Gareth Nicholson <gareth.nic@gmail.com>
2016-09-30 13:46:02 +0200

    frontend changes

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MoneyMath</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="css/app.css" rel="stylesheet">
+        <link href="css/slicknav.min.css" rel="stylesheet">
+
+        @yield('styles')
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+    <body>
+        @include('partials.navigation')
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+        @yield('scripts')
+
+        <script src="js/jquery.slicknav.min.js"></script>
+    </body>
+</html>
---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,12 @@
+<nav class="navBar">
+    <ul>
+        <li class="nav-item"><a href="/login">Login</a></li>
+        <li class="nav-item"><a href="/logout">Logout</a></li>
+    </ul>
+</nav>
+
+<script>
+    $(document).ready(function () {
+        $('.navBar').slicknav({label: ''});
+    });
+</script>
