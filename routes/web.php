8d1d608558a26bf6f373e45e78e5852dc41aadae
Gareth Nicholson <gareth.nic@gmail.com>
2016-10-01 22:32:10 +0200

    add get/set income/expense routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,13 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index');
+
 Route::get('/app', function () {
     return view('index');
 });
+
+Route::post('/add-income', 'AppController@addIncome');
+Route::get('/get-income', 'AppController@getIncome');
+
+Route::post('/add-expense', 'AppController@addExpense');
+Route::get('/get-expense', 'AppController@getExpense');
